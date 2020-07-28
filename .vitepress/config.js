fa16bfb758380c8de1a289591b2e639fa13259ba
runki <runki@dongyuinfo.com>
2020-07-28 10:55:15 +0800

    first commit

---
+++ .vitepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: '喵帕斯',
+    themeConfig:{
+        logo:'mps.png',
+        nav:[
+            {
+                text:'喵',
+                link:'nyan',
+            },
+            {
+                text:'帕',
+                link:'pa',
+            },
+            {
+                text:'斯',
+                link:'ss',
+            }
+        ],
+        sidebar: [
+            {
+              text: '起步',
+              link: 'getting-started',
+            }
+        ],
+        prevLink:true,
+        nextLink:true
+    }
+}
